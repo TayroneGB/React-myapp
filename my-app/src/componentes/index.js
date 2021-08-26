0a6454890d1444c52b4c24ebcd9c630dcdb7a7f6
Tayrone <tayrone.bez@gmail.com>
2021-08-26 18:34:46 -0300

    mnk

---
+++ my-app/src/componentes/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+const Component = () => {
+  return <p>Aleatorio</p>
+  
+}
+
+export default Component;
---
+++ my-app/src/componentes/panetas/index.js
@@ -0,0 +1,15 @@
+import React, {Fragment} from 'react';
+import 
+const Planets=()=>{
+    return(
+    <Fragment>
+        <h3>lista de planetas</h3>
+        <p>safmsalkf</p>
+        
+    
+    </Fragment>
+    )
+}
+
+
+export default Planets; 
